5d780664810179436b78a599b8bc1eb993b40ad8
Nuria Ruiz de Almirón Bartolomé <ruizdealmiron@gmail.com>
2022-12-10 21:31:20 +0100

    Cambios para realizar un indice del fichero de contaminantes

--- scripts/index.sh
+++ scripts/index.sh
@@ -1,3 +1,5 @@
+
+
 # This script should index the genome file specified in the first argument ($1),
 # creating the index in a directory specified by the second argument ($2).
 
@@ -6,3 +8,7 @@
 
 # STAR --runThreadN 4 --runMode genomeGenerate --genomeDir <outdir> \
 # --genomeFastaFiles <genomefile> --genomeSAindexNbases 9
+echo "Running STAR index..."
+mkdir -p res/contaminants_idx
+STAR --runThreadN 4 --runMode genomeGenerate --genomeDir res/contaminants_idx/ --genomeFastaFiles res/contaminants.fasta --genomeSAindexNbases 9
+echo
