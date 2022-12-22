3b8257afd45757184d35b95765caa5120ec9c6a6
Nuria Ruiz de Almirón Bartolomé <ruizdealmiron@gmail.com>
2022-12-22 22:06:46 +0100

    documentamos la mejora realizada en la descarga de las muestras

--- scripts/pipelinePlus.sh
+++ scripts/pipelinePlus.sh
@@ -1,11 +1,16 @@
 
-echo "-------- Starting pipeline at $(date +'%d %h %y, %r')... --------"
+echo "-------- Starting pipelinePlus at $(date +'%d %h %y, %r')... --------"
 
 #Download all the files specified in data/filenames
-#for url in $(<list_of_urls>) #MEJORA Download all the files in one liner
-#para ello en la misma carpeta muevo el fichero con las urls 
+#for url in $(<list_of_urls>)
+#MEJORA-BONUS Download all the files in one liner
+#
 echo "Download list of urls files."
 wget -P ~/decont/data -i data/urls
 echo " end download files" 
 ##
-## resto del scripts pipeline.sh quitando el bucle de descargas de las urls 
+## Incorporado en el scripts principal pipeline.sh -> quitando el bucle de descargas de las urls + cleanup.sh + Pruebas de nuevo
+## NOTA: Con esta mejora no descomprimimos los ficheros fasta, se trabaja con muestras comprimidas, por el elevado tamaño de los ficheros.
+## 
+echo "-------- Ended pipelinePlus at $(date +'%d %h %y, %r')... --------"
+
